a5bd8c0a70e003e3adc1e6d236b0b34ecf23e621
Christina Nguyen <christina.nguyen8711@gmail.com>
2024-07-24 21:01:09 -0400

    Starting Point for Portfolio Website

---
+++ src/Components/Navbar.js
@@ -0,0 +1,6 @@
+//React components are regular JavaScript functions, but their names must start with a capital letter or they won’t work!
+export default function Navbar(){
+    return(
+        
+    )
+}
